65a694b44ec797b1d975b502f2045a2464eed3bd
Kenya Mitchell <102193684+Spider-Man2099@users.noreply.github.com>
2022-11-26 06:21:59 +0000

    Finally got the chart to work and figured out the css aspects are controlled through the canvas. So created canvas tag, now can edit ;-;.  Also, added EMS function to package.json.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 import Heading from './components/Heading.jsx'
 import Navbar from './components/Navbar.jsx'
+import Money from './components/Money.jsx'
+
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+
 /**
 This is where the actual code being rendered in index.jsx is 
 import './components/Heading.jsx'
@@ -11,21 +15,20 @@ are named export with { }
 */
 
 export default function App() {
+
+   
   
   return (
 
     
-
-    <Router>
-
     
-     <Routes>
-       <Route path='/' element = {<Heading/>}/>
-     </Routes>
+     <div>
 
-     
+      <Heading/>
       <img src = "./public/helicopter.jpg"/> 
-   </Router>
+      <Money/>
+     </div>
+  
    
   );
 }
--- src/components/Money.jsx
+++ src/components/Money.jsx
@@ -1,8 +1,47 @@
 import '../App.css'
-import {CChart} from '@coreui/react-chartjs'; 
+import React, {useRef, useEffect} from 'react'; 
+import Chart from 'chart.js/auto'
+import {Line} from 'react-chartjs-2'
+import { CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
+const labels = ["January", "February", "March", "April", "May", "June"];
 
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "white",
+      borderColor: "red",
+      hoverBackgroundColor: "blue",
+      data: [0, 10, 5, 2, 20, 30, 45],
+    },
+  ],
+};
+
+const options = {
+   responsive: true,
+   options: {
+    plugins: {
+          legend: true, 
+      },
+    },
+};
 export default function Money(){
 
+  
+  return(
+    
+    <div>
+        <Line options={options} data={data} height={100}/>
+    </div>
+  );
   
 }
